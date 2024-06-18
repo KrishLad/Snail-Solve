0e56fd129079fdac4475101ab73be1544f13c2fc
Krishna Lad <krishnalad54@gmail.com>
2024-06-18 19:16:33 -0400

    GAME IS WORKING,(very rough) BUT WORKINGgit add Game.* Problem.css !

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import './Game.css'
 import Problem from "./Problem";
 import Timer from "./Timer";
@@ -7,6 +7,13 @@ function getRandomInt(max:number){
   return Math.floor(Math.random() * max);
 }
 
+function isValidInput(value: string) {
+ return /^\d+(\.\d+)?$/.test(value);
+}
+
+function clearInput(){
+  document.getElementById('answerBox').value = ""; //it will not be null.
+}
 
 const Game = () => {
   const [N1, setN1] = useState(getRandomInt(10));
@@ -25,32 +32,39 @@ const Game = () => {
     }
   } 
 
-  
-  const playGame = () =>{
-    // if this function is called and the user is alredy playing then noting should happen
-    if(!gameStarted){
-      startTimer();
+  useEffect(() => {
+    if(answer && !gameStarted){
       setGameStarted(true);
-      while(time != 0){
-        const ans = parseInt(answer);
-        if(ans === N1+N2){
-          setScore(score+1);
-        }
-        const n1 = getRandomInt(10);
-        const n2 = getRandomInt(10);
-        setN1(n1);
-        setN2(n2);
-      }
-      console.log(score);
+      startTimer();
     }
-  }
+    else if(answer){
+      if(parseInt(answer) === N1+N2){
+        setScore(score+1);
+        setN1(getRandomInt(10));
+        setN2(getRandomInt(10));
+        clearInput();
+      }
+    }
+    else{
+      if(gameStarted && time === 0 ){
+        setGameStarted(false);
+        console.log("Score is : " + score);
+        return;
+      }
+    }
+  }, [answer, gameStarted, time]);
+
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAnswer(e.target.value);
+    
+  };
+  
 
   return (
-    <> 
+    <>
       <div className="timer-box">
-        <div className="timer">
-         {timerStarted && render} 
-        </div>
+        <div className="timer">{timerStarted && render}</div>
       </div>
       <div className="game-content">
         <div className="problem">
@@ -58,12 +72,16 @@ const Game = () => {
         </div>
         <input
           type="in"
-          id = "answerBox"
+          id="answerBox"
           className="input"
-          placeholder="0"
-          maxLength={1}
-          onChange={playGame}
-          value={answer}
+          placeholder="00"
+          onChange={handleInputChange}
+          onKeyDown={(e) => {
+            if(!(e.code === 'Backspace' || isValidInput(e.key))){
+              e.preventDefault();
+            }
+          }}
+          maxLength={2}
         />
       </div>
     </>
